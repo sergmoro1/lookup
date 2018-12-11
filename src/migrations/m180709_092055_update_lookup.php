643c1937d87b51342d1f820c92c0bbb7a5fcd643
Sergey Morozov <sergmoro1@ya.ru>
2018-12-11 08:41:10 +0300

    replace \t

--- src/migrations/m180709_092055_update_lookup.php
+++ src/migrations/m180709_092055_update_lookup.php
@@ -10,7 +10,7 @@ use sergmoro1\lookup\models\Property;
  */
 class m180709_092055_update_lookup extends Migration
 {
-	const TABLE = '{{%lookup}}';
+    const TABLE = '{{%lookup}}';
 
     // Use up()/down() to run migration code without a transaction.
     public function up()
@@ -18,17 +18,17 @@ class m180709_092055_update_lookup extends Migration
         $this->addColumn(self::TABLE, 'property_id', $this->integer());
         $property = null;
         foreach(Lookup::find()->all() as $item) {
-			if(!($property && $property->name == $item->type)) {
-			    if(!($property = Property::findOne(['name' => $item->type]))) {
-			        $property = new Property([
-			            'name' => $item->type,
-			        ]);
-			        $property->save();
-			    }
-			}
-			$item->property_id = $property->id;
-			$item->save();
-		}
+            if(!($property && $property->name == $item->type)) {
+                if(!($property = Property::findOne(['name' => $item->type]))) {
+                    $property = new Property([
+                        'name' => $item->type,
+                    ]);
+                    $property->save();
+                }
+            }
+            $item->property_id = $property->id;
+            $item->save();
+        }
         $this->createIndex('property_code', self::TABLE, ['property_id', 'code']);
         $this->addForeignKey ('FK_lookup_property', self::TABLE, 'property_id', '{{%property}}', 'id', 'CASCADE');
         $this->dropColumn(self::TABLE, 'type');
@@ -38,9 +38,9 @@ class m180709_092055_update_lookup extends Migration
     {
         $this->addColumn(self::TABLE, 'type', $this->string(128));
         foreach(Lookup::find()->all() as $item) {
-			$item->type = $item->property->name;
-			$item->save();
-	    }
+            $item->type = $item->property->name;
+            $item->save();
+        }
         $this->dropIndex('property_code', self::TABLE);
         $this->dropIndex('FK_lookup_property', self::TABLE);
         $this->dropColumn(self::TABLE, 'property_id');
